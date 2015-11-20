04ea7f051ed999cf148502773f1d6154f6e286c2
Alex Clark <aclark2089@gmail.com>
2015-11-20 02:39:57 -0500

    Set to local

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -96,6 +96,18 @@ DATABASES = {
         'USER': 'admin',
         'PASSWORD': '***REMOVED***',
         'HOST': '***REMOVED***',
+        'PORT': '3306',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '***REMOVED***',
+        'USER': '***REMOVED***',
+        'PASSWORD': ***REMOVED***,
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
