ff5d5537469a481e3dca94833405a366e1d92362
Alex Clark <aclark2089@gmail.com>
2015-11-20 20:50:27 -0500

    config new serializer class models for each object

--- app/store/routes/viewsets.py
+++ app/store/routes/viewsets.py
@@ -6,22 +6,36 @@ from rest_framework import viewsets
 class UserList(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class UserDetail(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 # Product
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductList(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 # Order
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderList(viewsets.ReadOnlyModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDetail(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
 # Supplier
-class SupplierViewSet(viewsets.ModelViewSet):
+class SupplierList(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+class SupplierDetail(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
