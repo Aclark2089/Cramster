a2494e923fc5a01f18720e8639d6582d19dfd368
R. Alex Clark <aclark2089@gmail.com>
2015-12-04 19:33:31 -0500

    Fixed urls, need to fix csrf key

--- app/store/urls.py
+++ app/store/urls.py
@@ -14,12 +14,12 @@ urlpatterns = format_suffix_patterns([
     url(r'^accounts/$', views.user_list, name='user_list'),
 
     # Login urls
-    url(r'^accounts/login/', views.login, name='login'),
-    url(r'^accounts/auth/', views.auth_view, name='auth_view'),
-    url(r'^accounts/logout/', views.logout, name='logout'),
-    url(r'^accounts/invalid/', views.invalid_login, name='invalid_login'),
-    url(r'^accounts/register/', views.register_user, name='register_user'),
-    url(r'^accounts/register_success/', views.register_success, name='register_success'),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/auth/$', views.auth_view, name='auth_view'),
+    url(r'^accounts/logout/$', views.logout, name='logout'),
+    url(r'^accounts/invalid/$', views.invalid_login, name='invalid_login'),
+    url(r'^accounts/register/$', views.register_user, name='register_user'),
+    url(r'^accounts/register_success/$', views.register_success, name='register_success'),
 
     url(r'^accounts/settings/', views.settings, name='settings'),
     url(r'^accounts/delete/([0-9]+)/$', views.delete_user, name='delete_user'),
