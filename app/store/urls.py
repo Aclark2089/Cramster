44d8ef59f070d96f0f5bd484acdf9e39ff1bbee4
KimBro <kkimbroughjr@hotmail.com>
2015-12-05 00:10:41 -0500

    working on adding products

--- app/store/urls.py
+++ app/store/urls.py
@@ -8,6 +8,7 @@ urlpatterns = format_suffix_patterns([
     url(r'^search/$', views.search, name='search'),
 
     url(r'^products/$', views.product_catalog, name='product_catalog'),
+    url(r'^products/add/$', views.add_product, name='add_product'),
     url(r'^products/edit/([0-9]+)/$', views.edit_product, name='edit_product'),
     url(r'^products/delete/([0-9]+)/$', views.delete_product, name='delete_product'),
     url(r'^orders/$', views.orders, name='orders'),
