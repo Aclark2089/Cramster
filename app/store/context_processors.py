163e0e42b64c8a9a5542f46c0755fe2f2522658f
R. Alex Clark <aclark2089@gmail.com>
2015-12-05 20:39:47 -0500

    Added a context processor for a cart and added a cancel button to the checkout

--- app/store/context_processors.py
+++ app/store/context_processors.py
@@ -2,8 +2,10 @@ from .models import Product
 
 def store(request):
     galleryproducts = Product.objects.all()[:3]
+    cart = {}
     context = {
         "store_title": "Cramster",
-        "galleryproducts": galleryproducts
+        "galleryproducts": galleryproducts,
+        "cart": {},
     }
     return context
